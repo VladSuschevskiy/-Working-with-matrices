8b337207ab115a785fc294a4ef17ed7e925ae24e
Vlad Suschevskiy <rts123adc123@gmail.com>
2021-04-09 18:53:44 +0300

    create matrix

---
+++ index.js
@@ -0,0 +1,36 @@
+const matrixA = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 2, 9],
+];
+
+const matrixB = [
+    [5, 6, 7],
+    [8, 9, 10],
+    [11, 12, 13],
+];
+
+// подразумеваем что матрицы одинакового размера
+const combineMatrixes = (m1, m2, operator) => {
+    if (operator !== "+" && operator !== "-" && operator !== "*") {
+       return console.error(operator, "не поддерживается")
+    }
+
+    const matrixC = [];
+
+    for (let i = 0; i < m1.length; i++) {
+        for (let j = 0; j < m1[i].length; j++) {
+            if(operator === "+") {
+                matrixC.push(m1[i][j] + m2[i][j]) ;
+            } else if (operator === "-") {
+                matrixC.push(m1[i][j] - m2[i][j]) ;
+            } else if (operator === "*") {
+                matrixC.push(m1[i][j] * m2[i][j]) ;
+            }
+        }
+    }
+
+    return matrixC;
+}
+
+console.log(combineMatrixes(matrixA, matrixB, "+"));
